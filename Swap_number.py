8bdcb6f89ef177afe4fcc6779471d5ef3e056105
Muzammil  Khan <muzammilkhan199936@gmail.com>
2023-05-31 16:29:00 +0530

    All done!

---
+++ Swap_number.py
@@ -0,0 +1,12 @@
+a=int(input("Enter your Number:\n"))
+b=int(input("Enter Your Number:\n"))
+temp =a
+a=b
+b=temp
+
+print("\tSwapping Started\t")
+print("After Sawpping the number value a is",a)
+print("After Sawpping the number value b is",b)
+
+
+print("\tSwapping Successfully Done !\t ")
